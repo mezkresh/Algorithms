2d2f208adf36b21c48fd76873196cf1794f7e70f
mezkresh <evkartv@gmail.com>
2018-04-13 17:41:38 +0300

    modified quick sort java

--- sort java/Quick.java
+++ sort java/Quick.java
@@ -19,27 +19,26 @@ public class Quick{
 	}
 	
 	void quickSort(int[] a,int l,int r){
-	
-	int i = l;
-	int j = r;
-	int temp;
-	int pivot = a[(l+r)/2];
-	while(i<=j){
-		while(a[i]<pivot)
-			i++;
-		while(a[j]>pivot)
-			j--;
-		if(i<=j){
-			temp = a[i];
-			a[i] = a[j];
-			a[j] = temp;
-			i++;
-			j--;
+		int i = l;
+		int j = r;
+		int temp;
+		int pivot = a[(l+r)/2];
+		while(i<=j){
+			while(a[i]<pivot)
+				i++;
+			while(a[j]>pivot)
+				j--;
+			if(i<=j){
+				temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+				i++;
+				j--;
+			}
 		}
-	}
-	if(l<j)
-		quickSort(a,l,j);
-	if(i<r)
-		quickSort(a,i,r	);
+		if(l<j)
+			quickSort(a,l,j);
+		if(i<r)
+			quickSort(a,i,r	);
 	}
 }
